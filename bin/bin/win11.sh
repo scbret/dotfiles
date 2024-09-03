92b01936af6149fbfead68620388c97a44e67a3d
scbret <sbretey@gmail.com>
2024-09-03 12:10:38 -0500

    slg-arch

--- bin/bin/win11.sh
+++ bin/bin/win11.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 virsh --connect qemu:///system start win11
 sleep 30
-xfreerdp3 /u:sbret /p:93stratos39 /size:100% /v:192.168.122.248
+xfreerdp3 /u:sbret /p:93stratos39 /size:100% /sound /v:192.168.122.248
