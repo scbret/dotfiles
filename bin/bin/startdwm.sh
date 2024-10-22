9e0a5e4a6cddac77852c9cb591f44e3249490855
scbret <sbretey@gmail.com>
2024-10-22 13:35:52 -0500

    spectre-arco

---
+++ bin/bin/startdwm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+feh --bg-scale ~/wallpaper/mountain-stream.jpg
+
+
+while true; do
+    # Log stderror to a file
+    dwm 2> ~/.dwm.log
+    # No error logging
+    # dwm >/dev/null 2>&1
+done
