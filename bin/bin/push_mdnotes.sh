57bdfe2af53d6bf0d5cea6ef15f1b7d01c0ff0fa
scbret <sbretey@gmail.com>
2024-10-26 16:33:25 -0500

    spectre-arco

--- bin/bin/push_mdnotes.sh
+++ bin/bin/push_mdnotes.sh
@@ -3,11 +3,14 @@
 # Change directory to /repos/journal
 cd /home/steve/repos/linux/mdnotes || exit
 
+# get hostname of device being run on
+hostname=$(hostnamectl | awk -F ': ' /Static hostname/ {print $2}')
+
 # Add all changes to the staging area
 git add .
 
 # Commit changes
-git commit -m "slg-arch"
+git commit -m "$hostname"
 
 # Push changes to the remote repository
 git push
