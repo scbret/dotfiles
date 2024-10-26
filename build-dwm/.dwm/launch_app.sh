3d67d4adcd22583401621c8160e6d42dc7db72ac
scbret <sbretey@gmail.com>
2024-10-26 17:04:05 -0500

    slg-arch

--- build-dwm/.dwm/launch_app.sh
+++ build-dwm/.dwm/launch_app.sh
@@ -3,6 +3,14 @@
 # read the apps list text file
 apps=$(cat ~/.dwm/apps.txt)
 
-# pipe the list to dmenu
-dmenu -i -l 30 -p "Select an app:" <<< "$apps"
-notify-send "Your app was selected"
+# pipe the list to dmenu and capture selected app
+selected_app=$(echo "$apps" | dmenu -i -l 30 -p "Select an app:")
+
+# launch selected app if not empty
+if [ -n "$selected_app" ]; then
+	notify-send "Launching: $selected_app"
+	$selected_app &
+else
+	notify-send "No app selected"
+fi
+
--- build-dwm/.dwm/md-notes.sh
+++ build-dwm/.dwm/md-notes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-folder=/home/steve/repos/mdnotes/
+folder=/home/steve/repos/linux/mdnotes/
 
 newnote () { \
 	name="$(echo "" | dmenu -sb "#a3be8c" -nf "#d8dee9" -p "Enter a name: " <&- )"
