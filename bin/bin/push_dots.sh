1616be3dc28b97854409689298debdd554af2ddd
scbret <sbretey@gmail.com>
2024-10-26 16:42:20 -0500

    slg-arch

--- bin/bin/push_dots.sh
+++ bin/bin/push_dots.sh
@@ -3,11 +3,14 @@
 # Change directory to /repos/journal
 cd /home/steve/dotfiles || exit
 
+# get hostname of device being run on
+hostname=$(hostnamectl | awk -F ': ' '/Static hostname/ {print $2}')
+
 # Add all changes to the staging area
 git add .
 
 # Commit changes
-git commit -m "spectre-arco"
+git commit -m "$hostname"
 
 # Push changes to the remote repository
 git push
