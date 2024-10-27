5e79ce6bd32e82fe0d759f3fe37da4f9b680aac9
scbret <sbretey@gmail.com>
2024-10-26 19:41:02 -0500

    slg-arch

--- build-dwm/dmenu/config.def.h
+++ build-dwm/dmenu/config.def.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#65737e" },
+	[SchemeSel] = { "#eeeeee", "#673888" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
