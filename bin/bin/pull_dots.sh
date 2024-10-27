12bac8282bb40294018a43f00208dd4ea30dad71
scbret <sbretey@gmail.com>
2024-10-27 16:08:51 -0500

    slg-arch

--- bin/bin/pull_dots.sh
+++ bin/bin/pull_dots.sh
@@ -5,7 +5,7 @@ cd /home/steve/dotfiles || exit
 
 # Push changes to the remote repository
 git pull
-
+sleep 2
 # cd back to home
 cd  /home/steve || exit
 
--- bin/bin/push_dots.sh
+++ bin/bin/push_dots.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# Change directory to /repos/journal
+# Change directory to /dotfiles
 cd /home/steve/dotfiles || exit
 
 # get hostname of device being run on
 hostname=$(hostnamectl | awk -F ': ' '/Static hostname/ {print $2}')
-
+sleep 2
 # Add all changes to the staging area
 git add .
-
+sleep 2
 # Commit changes
 git commit -m "$hostname"
-
+sleep 4
 # Push changes to the remote repository
 git push
-
+sleep 2
 # cd back to home
 cd /home/steve || exit
 
