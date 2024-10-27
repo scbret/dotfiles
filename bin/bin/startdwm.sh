e2d95cea00bc12a04543da4ce6411af284f48b83
scbret <sbretey@gmail.com>
2024-10-27 12:49:23 -0500

    spectre-arco

--- bin/bin/startdwm.sh
+++ bin/bin/startdwm.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/sh
 
+if [ -d /etc/X11/xinit/xinitrc.d ]; then
+      for f in /etc/X11/xinit/xinitrc.d/*; do
+        [ -x "$f" ] && . "$f"
+     done
+     unset f
+ fi
+
 feh --bg-scale ~/wallpaper/mountain-stream.jpg
-dwmblocks &
+
 exec picom --config ~/.config/picom-dwm/picom.conf &
+
+slstatus &
+dunst &
+
+xset dpms 300 &
+xss-lock -- slock &
 
 while true; do
     # Log stderror to a file
