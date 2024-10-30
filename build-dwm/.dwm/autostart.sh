1b1dc705b600769f1cd03ea4a21cfb82f2ebd333
scbret <sbretey@gmail.com>
2024-10-30 14:30:06 -0500

    slg-arch

--- build-dwm/.dwm/autostart.sh
+++ build-dwm/.dwm/autostart.sh
@@ -1,8 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
+#slstatus &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# backgrounf
+feh --bg-scale ~/wallpaper/pfback-titles.jpg &
+
+picom --animations -b &
+
+# One Drive 
 sh -c "rclone --vfs-cache-mode writes mount \"onedrive\": ~/OneDrive"
+
 
 #while true; do
 #	xsetroot -name "**** SwineID ****"
 #	sleep 60
 #done &
+
+dunst &
