74892acd1fcdf73d082f5a6e28b3e0a87a5fb9f5
scbret <sbretey@gmail.com>
2024-11-09 16:07:02 -0600

    slg-arch

--- build-dwm/.dwm/keyhelper.sh
+++ build-dwm/.dwm/keyhelper.sh
@@ -1,37 +1,7 @@
 #!/usr/bin/env bash
 
-# Terminal styling variables
-bold=$(tput bold)
-normal=$(tput sgr0)
+# read the keybinds text file
+kbinds=$(cat ~/dotfiles/key_binds_dwm.txt)
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
-
-# Path to the dotfiles folder
-folder=~/dotfiles
-
-# Path to the custom text file inside the dotfiles folder
-custom_bindings_file="$folder/key_binds_dwm.txt"
-
-# Check if the custom file exists
-if [ -f "$custom_bindings_file" ]; then
-    # Open the file using st and less with customized prompt
-    st -e bash -c "
-        cat \"$custom_bindings_file\" | {
-            while IFS= read -r line; do
-                if [[ \"\$line\" == \"\${line^^}\" ]]; then
-                    echo -e \"${RED}\$line${NC}\"
-                else
-                    echo \"\$line\"
-                fi
-            done
-        } | less -R -P '[more]'" 
-else
-    echo "Custom bindings file not found: $custom_bindings_file"
-fi
-
-
+# pipe the list to dmenu
+echo "$kbinds" | dmenu -i -l 30
