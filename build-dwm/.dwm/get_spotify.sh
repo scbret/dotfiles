5d216eba95b22f4ab55caa7f9a5be22b50499a38
scbret <sbretey@gmail.com>
2024-11-29 10:13:19 -0600

    spectre

--- build-dwm/.dwm/get_spotify.sh
+++ build-dwm/.dwm/get_spotify.sh
@@ -2,7 +2,7 @@
 
 while true; do
    if playerctl -p spotify status 2>/dev/null | grep -q "Playing"; then
-       spotify_title=$(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')
+       spotify_title=$(playerctl -p spotify metadata --format '{{ artist }} - {{ album }} - {{ title }}')
        printf "%s\n" "$spotify_title"
    else
        printf "%s\n" "Not Currently Playing"
