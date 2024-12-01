901ad4dec7e068619f76626234010e2d4f9c4534
scbret <sbretey@gmail.com>
2024-12-01 13:16:26 -0600

    slg-arch

--- build-dwm/.dwm/get_spotify.sh
+++ build-dwm/.dwm/get_spotify.sh
@@ -5,7 +5,7 @@ while true; do
        spotify_title=$(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')
        printf "%s\n" "$spotify_title"
    else
-       printf "%s\n" ""
+       printf "%s\n" "󰝛"
    fi
    sleep 5
    clear
