424f0ef14a495106ce00f6e38a2c067fb61baa16
scbret <sbretey@gmail.com>
2024-12-03 17:16:04 -0600

    spectre

--- spectre/arco-dwm/autostart.sh
+++ spectre/arco-dwm/autostart.sh
@@ -13,8 +13,13 @@ run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
 #run xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
-run xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+###run xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+##run xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal --above eDP1
+
+run xrandr --output eDP1 --off --output DP1 --off --output DP2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
 #run xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #run xrandr --output DVI-I-0 --right-of HDMI-0 --auto
 #run xrandr --output DVI-1 --right-of DVI-0 --auto
@@ -40,7 +45,8 @@ feh --bg-fill $HOME/wallpaper/arco-wallpaper.jpg &
 #feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #run applications from startup
 
-run "conky -c $HOME/.config/arco-dwm/system-keybinds"
+#run "conky -c $HOME/.config/arco-dwm/system-keybinds"
+run "conky -c $HOME/.config/arco-dwm/system-monitor"
 
 #Auto mount OneDrive
 sh -c "rclone --vfs-cache-mode writes mount \"onedrive\": ~/OneDrive"
