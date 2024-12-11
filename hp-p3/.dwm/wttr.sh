86e56272af0d23a3b0d814a2f6716344afa2d3a4
scbret <sbretey@gmail.com>
2024-12-11 09:05:21 -0600

    TSP3

--- hp-p3/.dwm/wttr.sh
+++ hp-p3/.dwm/wttr.sh
@@ -4,6 +4,6 @@
 if curl -s "wttr.in/mason+city?u&format=%m+%C,+H:%h,+A:%t,+F:%f,+W:%w\n" > /tmp/wttr; then
     notify-send "Weather data successfully retrieved and saved"
 else
-    echo "Error: Failed to retrieve weather data" > /tmp/wttr
+    echo "Failed to retrieve weather data" > /tmp/wttr
     exit 1
 fi
