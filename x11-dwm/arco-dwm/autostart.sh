70e30aba70e4fd7152f728a1643a682ba56e3327
scbret <sbretey@gmail.com>
2025-01-04 15:15:33 -0600

    X1

--- x11-dwm/arco-dwm/autostart.sh
+++ x11-dwm/arco-dwm/autostart.sh
@@ -55,7 +55,7 @@ feh --bg-fill $HOME/wallpaper/baloon.jpg &
 run "conky -c $HOME/.config/arco-dwm/system-monitor"
 
 #Auto mount OneDrive
-sh -c "rclone --vfs-cache-mode writes mount \"onedrive\": ~/OneDrive"
+#sh -c "rclone --vfs-cache-mode writes mount \"onedrive\": ~/OneDrive"
 
 #run "insync start"
 #run "spotify"
