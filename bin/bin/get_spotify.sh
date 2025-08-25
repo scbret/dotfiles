927838eb28a8cdfd031ae3e562886b8784477a60
scbret <sbretey@gmail.com>
2025-08-25 06:53:23 -0500

    HP3

---
+++ bin/bin/get_spotify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+   if playerctl -p spotify status 2>/dev/null | grep -q "Playing"; then
+       spotify_title=$(playerctl -p spotify metadata --format '{{ artist }} - {{ album }} - {{ title }}')
+       printf "%s\n" "$spotify_title"
+   else
+       printf "%s\n" "󰝛"
+   fi
+   sleep 5
+   clear
+done
