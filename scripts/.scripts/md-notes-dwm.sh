a36260b315c5e36d9b8ec3f6ac6f5cab4024c841
scbret <sbretey@gmail.com>
2025-10-19 09:28:11 -0500

    slg

--- scripts/.scripts/md-notes-dwm.sh
+++ scripts/.scripts/md-notes-dwm.sh
@@ -4,14 +4,14 @@ folder=/home/steve/repos/linux/mdnotes/
 
 newnote () { \
 	name="$(echo "" | dmenu -sb "#a3be8c" -nf "#d8dee9" -p "Enter a name: " <&- )"
-	setsid -f "kitty" -e nvim $folder$name".md" >/dev/null 2>&1
+	setsid -f "st" -e nvim $folder$name".md" >/dev/null 2>&1
 }
 
 selected () { \
 	choice=$(echo -e "New\n$(ls -t1 $folder)" | dmenu -l 12 -i -p "Choose note or create new: ")
 	case $choice in
 		New) newnote ;;
-		*md) setsid -f "kitty" -e nvim "$folder$choice" >/dev/null 2>&1 ;;
+		*md) setsid -f "st" -e nvim "$folder$choice" >/dev/null 2>&1 ;;
 		*) exit ;;
 	esac
 }
