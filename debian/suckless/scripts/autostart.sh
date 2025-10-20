ab9194837acb0dd28b867ebb7e518f31bde7bbeb
scbret <sbretey@gmail.com>
2025-10-20 16:18:23 -0500

    slg

--- debian/suckless/scripts/autostart.sh
+++ debian/suckless/scripts/autostart.sh
@@ -7,6 +7,9 @@ lxpolkit &
 
 
 # background
+# wal generates color scheme off the named wallpaper without setting the wallpaper - let feh do that
+#  wal -i ~/.config/suckless/wallpaper/wallhaven-3q9vmd_3440x1440.png -n &
+
 #feh --bg-scale ~/.config/suckless/wallpaper/wallhaven-218x7m_3440x1440.png &
 #feh --bg-scale ~/.config/suckless/wallpaper/wallhaven-9mq26d_3440x1440.png &
 feh --bg-scale ~/.config/suckless/wallpaper/wallhaven-3q9vmd_3440x1440.png &
@@ -20,6 +23,6 @@ if hash sxhkd >/dev/null 2>&1; then
 	sleep 0.5
 	sxhkd -c "$HOME/.config/suckless/sxhkd/sxhkdrc" &
 fi
-
+xfce4-power-manager &
 dunst -config ~/.config/suckless/dunst/dunstrc &
 picom --config ~/.config/suckless/picom/picom.conf --animations -b &
