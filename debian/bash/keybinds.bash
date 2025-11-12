aacd6f8d4b2ccd6560cee8358fff5dec8eb5a517
scbret <sbretey@gmail.com>
2025-11-12 07:16:50 -0600

    slg

--- debian/bash/keybinds.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Custom keybindings
-
-# Ctrl+L to clear screen
-bind '"\C-l": clear-screen'
-
-# Up/Down arrow for history search
-bind '"\e[A": history-search-backward'
-bind '"\e[B": history-search-forward'
--- debian/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return 
-	{
-        'projekt0n/github-nvim-theme',
-        config = function()
-           vim.cmd.colorscheme("github_dark_default")
-        end,
-    }	
- 
